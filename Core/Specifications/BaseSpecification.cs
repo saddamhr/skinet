086fb19c45b2ff50c5085a2000b7ebd9ed80a6d8
Md_Saddam_Hossain <saddam.hossain@selise.ch>
2023-07-17 13:56:56 +0600

    End of Section 6

--- Core/Specifications/BaseSpecification.cs
+++ Core/Specifications/BaseSpecification.cs
@@ -20,6 +20,12 @@ namespace Core.Specifications
         public Expression<Func<T, object>> OrderBy {get; private set;}
         public Expression<Func<T, object>> OrderByDescending {get; private set;}
 
+        public int Take {get; private set;}
+
+        public int Skip {get; private set;}
+
+        public bool IsPagingEnabled {get; private set;}
+
         protected void AddInclude(Expression<Func<T, object>> includeExpression)
         {
             Includes.Add(includeExpression);
@@ -33,6 +39,13 @@ namespace Core.Specifications
         protected void AddOrderByDescending(Expression<Func<T, object>> orderByDscExpression)
         {
             OrderByDescending = orderByDscExpression;
+        }
+
+        protected void ApplyPaging(int skip, int take)
+        {
+            Skip = skip;
+            Take = take;
+            IsPagingEnabled = true;
         }
     }
 }
