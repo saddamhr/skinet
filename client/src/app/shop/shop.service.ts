ff7d4a17da8059ab9e090346c688228a4978dcf9
Md_Saddam_Hossain <saddam.hossain@selise.ch>
2023-07-21 12:46:26 +0600

    End of Section 10

--- client/src/app/shop/shop.service.ts
+++ client/src/app/shop/shop.service.ts
@@ -23,11 +23,15 @@ export class ShopService {
     params = params.append('sort', shopParams.sort);
     params = params.append('pageIndex', shopParams.pageNumber);
     params = params.append('pageSize', shopParams.pageSize);
-    if(shopParams.search) params = params.append('search', shopParams.search);
+    if (shopParams.search) params = params.append('search', shopParams.search);
 
     return this.http.get<Pagination<Product[]>>(`${this.baseUrl}products`, {
       params,
     });
+  }
+
+  getProduct(id: number) {
+    return this.http.get<Product>(`${this.baseUrl}products/${id}`);
   }
 
   getBrands() {
