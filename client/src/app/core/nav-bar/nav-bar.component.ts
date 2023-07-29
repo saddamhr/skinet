853e1cc24a584dec8f6e40ab1d63221bc932f556
Md_Saddam_Hossain <saddam.hossain@selise.ch>
2023-07-29 13:47:35 +0600

    End of section 14

--- client/src/app/core/nav-bar/nav-bar.component.ts
+++ client/src/app/core/nav-bar/nav-bar.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { BasketService } from '../../basket/basket.service';
+import { BasketItem } from '../../shared/models/basket';
 
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
-  styleUrls: ['./nav-bar.component.scss']
+  styleUrls: ['./nav-bar.component.scss'],
 })
 export class NavBarComponent {
+  constructor(public basketService: BasketService) {}
 
+  getCount(items: BasketItem[]) {
+    return items.reduce((sum, item) => sum + item.quantity, 0);
+  }
 }
--- client/src/app/shop/product-details/product-details.component.ts
+++ client/src/app/shop/product-details/product-details.component.ts
@@ -3,6 +3,8 @@ import { Product } from '../../shared/models/product';
 import { ShopService } from '../shop.service';
 import { ActivatedRoute } from '@angular/router';
 import { BreadcrumbService } from 'xng-breadcrumb';
+import { BasketService } from '../../basket/basket.service';
+import { take } from 'rxjs';
 
 @Component({
   selector: 'app-product-details',
@@ -11,13 +13,16 @@ import { BreadcrumbService } from 'xng-breadcrumb';
 })
 export class ProductDetailsComponent implements OnInit {
   product?: Product;
+  quantity = 1;
+  quantityInBasket = 0;
 
   constructor(
     private shopService: ShopService,
     private activatedRoute: ActivatedRoute,
-    private bcService: BreadcrumbService
+    private bcService: BreadcrumbService,
+    private basketService: BasketService
   ) {
-    this.bcService.set('@productDetails', ' ')
+    this.bcService.set('@productDetails', ' ');
   }
 
   ngOnInit(): void {
@@ -31,8 +36,43 @@ export class ProductDetailsComponent implements OnInit {
         next: (product) => {
           this.product = product;
           this.bcService.set('@productDetails', product.name);
+          this.basketService.basketSource$.pipe(take(1)).subscribe({
+            next: (basket) => {
+              const item = basket?.items.find((x) => x.id === +id);
+              if (item) {
+                this.quantity = item.quantity;
+                this.quantityInBasket = item.quantity;
+              }
+            },
+          });
         },
         error: (error) => console.log(error),
       });
+  }
+
+  incrementQuantity() {
+    this.quantity++;
+  }
+
+  decrementQuantity() {
+    this.quantity--;
+  }
+
+  updateBasket() {
+    if (this.product) {
+      if (this.quantity > this.quantityInBasket) {
+        const itemToAdd = this.quantity - this.quantityInBasket;
+        this.quantityInBasket += itemToAdd;
+        this.basketService.addItemToBasket(this.product, itemToAdd);
+      } else {
+        const itemsToRemove = this.quantityInBasket - this.quantity;
+        this.quantityInBasket -= itemsToRemove;
+        this.basketService.removeItemFromBasket(this.product.id, itemsToRemove);
+      }
+    }
+  }
+
+  get buttonText() {
+    return this.quantityInBasket === 0 ? 'Add to basket' : 'Update basket';
   }
 }
